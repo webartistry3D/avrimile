ae35799de7f8a30e774bb0cedb13949bfa03e743
webartistry3D <webartistry.io@gmail.com>
2025-08-18 17:13:16 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   const host = process.platform === "win32" ? "127.0.0.1" : "0.0.0.0";
 
   // ✅ Removed reusePort, not supported on Windows
-  server.listen(port, host, () => {
+  server.listen(port, "0.0.0.0", () => {
     log(`🚀 Serving on http://${host}:${port}`);
   });
 })();
