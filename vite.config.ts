e3d33fe69a555f0039a0254ed7f2eff591f57a28
WebArtistry.3D <webartistry.io@gmail.com>
2025-08-20 14:48:29 +0100

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/",  // repo name
+  base: "./",  // repo name
   plugins: [
     react(),
     runtimeErrorOverlay(),
